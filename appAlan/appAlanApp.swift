371f90a52a0c358fcbf7e9e0159efc7992ec8548
Alan <alanohayon@gmail.com>
2024-01-24 19:13:44 +0100

    Initial Commit

---
+++ appAlan/appAlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  appAlanApp.swift
+//  appAlan
+//
+//  Created by Alan Ohayon on 24/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct appAlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
